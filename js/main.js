2957a0c1681486b1f45302dc50c3f9180315871e
Yuma Nakao <nakaosolution@Gmail.com>
2021-01-18 15:17:11 +0900

    ドキュメントを記述

--- js/main.js
+++ js/main.js
@@ -6,14 +6,15 @@ var app = new Vue({
     loading: true
   },
   mounted: function() {
+    // APIよりJSONを取得しbpiに代入、エラーがあればエラー表示、ロードするまではローディング表示。
   	axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(function(response){
-        console.log(response.data.bpi)
-        console.log(response.data.bpi.USD.rate_float)
+        // console.log(response.data.bpi)
+        // console.log(response.data.bpi.USD.rate_float)
         this.bpi = response.data.bpi
-      }.bind(this))
+      }.bind(this)) //bind(this)がない場合thisはWindowsオブジェクトを意味する。bind(this)を指定するとVueインスタンスを意味する。
       .catch(function(error){
-      	console.log(error)
+      	//console.log(error)
         this.hasError =true
       }.bind(this))
       .finally(function(){
@@ -21,6 +22,7 @@ var app = new Vue({
       }.bind(this))
   },
   filters: {
+    // 小数点2桁まで表示
   	currencyDecimal(value) {
     	return value.toFixed(2)
     }
